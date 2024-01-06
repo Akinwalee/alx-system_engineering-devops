0cbd82ecb78c261f1e174cb8e5d8167c1d8e575a
Akinwalee <obatulafuad@gmail.com>
2024-01-06 18:39:55 +0000

    Updated files

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # matches h......
  
-sender = ARGV[0].scan(/from:([\W]?[\d|\w]*)/).join
-receiver = ARGV[1].scan(/to:(\W?[\d|\w]*)/).join
-flags = ARGV[2].scan(/flags:([\d\W]*)]/).join
+sender = ARGV[0].scan(/from:([\w.-]+)/).join
+receiver = ARGV[1].scan(/to:([\w.-]+)/).join
+flags = ARGV[2].scan(/flags:([\w\d\W]+)/).join
 
 puts "#{sender},#{receiver},#{flags}"
