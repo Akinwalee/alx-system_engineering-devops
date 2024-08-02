6540c2156bd9bd490472892419a8dea4f3f6aa17
Akinwalee <obatulafuad@gmail.com>
2024-08-02 02:38:40 +0000

    Updated files

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Python scrpts to gather data from an API"""
+
+
+import requests
+import sys
+
+
+employee_id = int(sys.argv[1])
+employee = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id)).json()
+tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos"
+        .format(employee_id)).json()
+
+count = 0
+complete = []
+name = employee["name"]
+
+for task in tasks:
+    if task["completed"] is True:
+        count += 1
+        complete.append(task["title"])
+
+print(f"Employee {name} is done with tasks({count}/{len(tasks)})")
+for title in complete:
+    print(f"\t {title}")
