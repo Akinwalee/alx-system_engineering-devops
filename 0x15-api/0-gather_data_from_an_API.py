142634add61b67f079d890871d9de1f7b2f5b47d
Akinwalee <obatulafuad@gmail.com>
2024-08-02 02:41:12 +0000

    Updated files

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" Python scrpts to gather data from an API"""
-
+"""
+Python scrpts to gather data from an API 
+This script gets an employee detials from the API
+and display as specified
+"""
 
 import requests
 import sys
