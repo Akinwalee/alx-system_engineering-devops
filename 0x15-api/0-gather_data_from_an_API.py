ceb7ad4ac1664430430f3c662819efb53a61482a
Akinwalee <obatulafuad@gmail.com>
2024-08-02 03:04:25 +0000

    Updated files

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,23 +8,24 @@ import requests
 import sys
 
 
-employee_id = int(sys.argv[1])
-employee = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}"
-        .format(employee_id)).json()
-tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos"
-        .format(employee_id)).json()
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    employee = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}"
+            .format(employee_id)).json()
+    tasks = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}/todos"
+            .format(employee_id)).json()
 
-count = 0
-complete = []
-name = employee["name"]
+    count = 0
+    complete = []
+    name = employee["name"]
 
-for task in tasks:
-    if task["completed"] is True:
-        count += 1
-        complete.append(task["title"])
+    for task in tasks:
+        if task["completed"] is True:
+            count += 1
+            complete.append(task["title"])
 
-print(f"Employee {name} is done with tasks({count}/{len(tasks)})")
-for title in complete:
-    print(f"\t {title}")
+    print(f"Employee {name} is done with tasks({count}/{len(tasks)})")
+    for title in complete:
+        print(f"\t {title}")
