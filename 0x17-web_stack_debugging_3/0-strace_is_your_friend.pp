9ef8d703eb45b911682c44cc26f9b336d7dfaad3
Akinwalee <obatulafuad@gmail.com>
2024-08-15 04:11:53 +0000

    Updated files

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#Ensure the WordPress directory has the correct permissions
+# Ensure that the WordPress directory has correct permissions
 file { '/var/www/html/wordpress':
   ensure  => directory,
   owner   => 'www-data',
@@ -7,8 +7,17 @@ file { '/var/www/html/wordpress':
   recurse => true,
 }
 
-# Ensure Apache service is running
+# Ensure the Apache configuration is correct (for example, setting correct ownership)
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+
+# Restart Apache to apply any changes
 service { 'apache2':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
+  require => File['/var/www/html/wordpress'],
 }
