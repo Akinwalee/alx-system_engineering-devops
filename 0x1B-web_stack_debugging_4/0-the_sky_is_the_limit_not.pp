2f5eee85f6399e1ee0b73a7992731ee631440746
Akinwalee <obatulafuad@gmail.com>
2024-08-23 01:23:12 +0000

    Updated files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 # Avoid high number of requests
 
 exec {'replace':
-	provider => shell,
-	command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-	before   => Exec['restart'],
-	}
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+  }
 
 exec {'restart':
-	provider => shell,
-	command  => 'sudo service nginx restart',
-	}
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  }
 
